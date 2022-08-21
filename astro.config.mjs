53dd7f7c4b39bbcd3277b72bb68b57cfbe4aae66
Maxi Ferreira <charca@gmail.com>
2022-08-21 10:53:41 -0700

    Index and movie pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'server',
+})
