a0d24ddba4d98798d18de27638433980d3d50992
Maxi Ferreira <charca@gmail.com>
2022-08-22 11:37:27 -0700

    Bunch of updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
+import netlify from '@astrojs/netlify/edge-functions'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
+  adapter: netlify(),
 })
