da275022f5a7ac6b44de12a2d07a10763e379a59
Maxi Ferreira <charca@gmail.com>
2022-08-22 11:46:58 -0700

    Switched back to regular netlify functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
-import netlify from '@astrojs/netlify/edge-functions'
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
