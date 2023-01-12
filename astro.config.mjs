ca366fef2b6582bdd96de78c279ece991e257b26
Maxi Ferreira <charca@gmail.com>
2023-01-12 12:06:26 -0800

    Updated to directory mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import cloudflare from '@astrojs/cloudflare'
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({ mode: 'directory' }),
 })
