f1a39b6201cca8fcafc80a69ad6f180dc0bc762b
Maxi Ferreira <charca@gmail.com>
2024-01-27 10:25:03 -0800

    Use static renderer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
-import cloudflare from '@astrojs/cloudflare'
+// import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: cloudflare({ mode: 'advanced' }),
+  output: 'static',
+  // adapter: cloudflare({ mode: 'advanced' }),
   vite: {
     define: {
       'process.env.TMDB_API_KEY': JSON.stringify(process.env.TMDB_API_KEY),
